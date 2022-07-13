7ac5731c8f1ed6c5e791484170b26456524edcdc
NamesJoeyWheeler <36152493+NamesJoeyWheeler@users.noreply.github.com>
2022-07-14 02:03:02 +1000

    Add files via upload

---
+++ assets/www/lang/spanish.js
@@ -0,0 +1,99 @@
+window.spanish = {};
+
+window.spanish.GAME_LANGUAGE = "spanish";
+
+window.spanish.TITLE = "Villainous";
+window.spanish.GAME_TITLE = "Black Hat Bots";
+
+window.spanish.LEVEL = "Level";
+
+window.spanish.CLOSE_DIALOG_MSG = "Do you want to leave?";
+window.spanish.CLOSE_DIALOG_YES = "Yes";
+window.spanish.CLOSE_DIALOG_NO = "No";
+
+window.spanish.MISSION_RUN_X_KM = "Run # kilometres";
+window.spanish.MISSION_DESTROY_X_ENEMIES = "Destroy # enemies";
+window.spanish.MISSION_DIE_X_TIMES = "Explode # times";
+window.spanish.MISSION_BOUNCE_IN_ENEMIES_X_TIMES = "Bounce on enemies # times";
+window.spanish.MISSION_COLLECT_X_COINS = "Collect # coins";
+window.spanish.MISSION_BLOCK_ATTACKS_X_TIMES = "Block attacks # times";
+window.spanish.MISSION_TEAR_ENEMIES_X_TIMES = "Strike enemies head-on # times";
+window.spanish.MISSION_SEND_TO_FLY_ENEMIES_X_TIMES = "Send enemies to fly # times";
+window.spanish.MISSION_CRUSH_ENEMIES_X_TIMES = "Crush enemies # times";
+window.spanish.MISSION_DESTROY_X_JAKES = "Destroy # Robo-Jakes";
+window.spanish.MISSION_DESTROY_X_FINNS = "Destroy # Robo-Finns";
+window.spanish.MISSION_DESTROY_X_BUBBLES = "Destroy # Robo-Bubbles";
+window.spanish.MISSION_DESTROY_X_BUTTERCUPS = "Destroy # Robo-Buttercups";
+window.spanish.MISSION_DESTROY_X_BLOSSOMS = "Destroy # Robo-Blossoms";
+window.spanish.MISSION_DESTROY_X_STEVENS = "Destroy # Robo-Steven";
+window.spanish.MISSION_COUNTER_DAMAGE_X_TIMES = "Counter-attack enemies # times";
+
+window.spanish.MISSION_KILL_SUNBLAST = "Defeat SunBlast";
+
+window.spanish.MISSION_USE_DEMENTIA_X_TIMES = "Use Demencia # times";
+window.spanish.MISSION_USE_JEKYLL_X_TIMES = "Use Jekyll # times";
+window.spanish.MISSION_USE_MUSCLEMAN_X_TIMES = "Use MuscleMan # times";
+window.spanish.MISSION_USE_SENTINEL_X_TIMES = "Use Sentinel # times";
+//
+window.spanish.TAB_MISSIONS = "Missions";
+window.spanish.TAB_UPGRADES = "Upgrades";
+window.spanish.TAB_HATS = "Hats";
+
+window.spanish.SHOP_ATTACK = "ATTACK";
+window.spanish.SHOP_UPPERCUT = "UPPERCUT";
+window.spanish.SHOP_SLAM = "SLAM";
+window.spanish.SHOP_DEFENSE = "DEFENSE";
+window.spanish.SHOP_ENERGY = "ENERGY";
+window.spanish.SHOP_LUCK = "LUCK";
+window.spanish.SHOP_COUNTER = "COUNTER";
+window.spanish.SHOP_MAGNET = "MAGNET";
+window.spanish.SHOP_SENTINEL = "SENTINEL";
+window.spanish.SHOP_JEKYLL = "JEKYLL";
+window.spanish.SHOP_DEMENTIA = "DEMENCIA";
+window.spanish.SHOP_MUSCLE = "MUSCLE";
+
+window.spanish.HAT_0 = "BHB";
+window.spanish.HAT_0_DESC_1 = "BHB"
+window.spanish.HAT_0_DESC_2 = "BHB"
+window.spanish.HAT_1 = "DEMENCIA";
+window.spanish.HAT_1_DESC_1 = "Increases speed\nof attacks";
+window.spanish.HAT_1_DESC_2 = "";
+window.spanish.HAT_2 = "5.0.5.";
+window.spanish.HAT_2_DESC_1 = "Increases\n defence";
+window.spanish.HAT_2_DESC_2 = "";
+window.spanish.HAT_3 = "FLUG";
+window.spanish.HAT_3_DESC_1 = "Fries the circuits\nof the enemies";
+window.spanish.HAT_3_DESC_2 = "";
+window.spanish.HAT_4 = "Grim";
+window.spanish.HAT_4_DESC_1 = "Increases damage\nof attack";
+window.spanish.HAT_4_DESC_2 = "";
+window.spanish.HAT_5 = "ETERNAL KNIGHT";
+window.spanish.HAT_5_DESC_1 = "Critical damage\nmore often";
+window.spanish.HAT_5_DESC_2 = "";
+window.spanish.HAT_6 = "EMERALD";
+window.spanish.HAT_6_DESC_1 = "Increases damage\nof uppercut";
+window.spanish.HAT_6_DESC_2 = "";
+window.spanish.HAT_7 = "LORD BOXMAN";
+window.spanish.HAT_7_DESC_1 = "Increases damage\nof slam";
+window.spanish.HAT_7_DESC_2 = "";
+window.spanish.HAT_8 = "MOJO JOJO";
+window.spanish.HAT_8_DESC_1 = "Power-ups\nmore often";
+window.spanish.HAT_8_DESC_2 = "";
+window.spanish.HAT_9 = "ANTI-POPS";
+window.spanish.HAT_9_DESC_1 = "Increases power\nof coin magnet";
+window.spanish.HAT_9_DESC_2 = "";
+window.spanish.HAT_10 = "BLACK HAT";
+window.spanish.HAT_10_DESC_1 = "Infinite\npower";
+window.spanish.HAT_10_DESC_2 = "";
+window.spanish.HAT_11 = "ICE KING";
+window.spanish.HAT_11_DESC_1 = "Chance of freezing\nthe enemy";
+window.spanish.HAT_11_DESC_2 = "";
+window.spanish.HAT_12 = "AKU";
+window.spanish.HAT_12_DESC_1 = "Increases damage\nof the counter";
+window.spanish.HAT_12_DESC_2 = "";
+
+window.spanish.TERMS_OF_USE = "https://www.cartoonnetwork.com.ar/terminos-de-uso";
+window.spanish.PRIVACY_POLICY = "https://www.cartoonnetwork.com.ar/politica-de-privacidad";
+
+window.spanish.VIDEO_INTRO = "intro_es";
+window.spanish.VIDEO_ENDING = "ending_es";
